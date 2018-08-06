04a9b660ab0effa6f7033dd62ac7754a865a3920
Roxanne Cojocariu <rmcojocariu@gmail.com>
2018-08-06 19:10:27 -0400

    fetch happy quotes from external api

---
+++ app/javascript/react/containers/HappyQuoteContainer.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { browserHistory } from 'react-router'
+
+class HappyQuoteContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      body: '',
+      author: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://quotes.rest/quote/search.json?category=happiness', {
+      headers: { 'X-TheySaidSo-Api-Secret':'4_VSex0_jq_Nu5GN94HewAeF' },
+      cache: 'no-store'
+    })
+    .then(response => {
+      if(response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+        error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => response.json())
+    .then(response => {
+      this.setState({
+        body: response.contents.quote,
+        author: response.contents.author
+      })
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+  render(){
+    return(
+      <div>
+        {this.state.body}<br />
+        {this.state.author}
+      </div>
+    )
+  }
+}
+
+export default HappyQuoteContainer
