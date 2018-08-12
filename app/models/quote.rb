b6dc1f520286f3a0b6bb77668c16a9f10f38ed26
Roxanne Cojocariu <rmcojocariu@gmail.com>
2018-08-12 11:42:12 -0400

    create favorited_quotes route with proper associations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,7 @@
 class Quote < ApplicationRecord
+  has_many :users, through: :favorited_quotes
+
+
   validates :mood, presence: true
   validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :fetched_quotes
       resources :quotes
+      resources :favorited_quotes
     end
   end
 
