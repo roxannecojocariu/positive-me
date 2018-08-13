33dbc955838ea9d37e507f5969ff17ffc0c01d32
Roxanne Cojocariu <rmcojocariu@gmail.com>
2018-08-12 21:23:27 -0400

    create conditionals for css stylings

--- app/javascript/react/containers/FetchedQuoteContainer.js
+++ app/javascript/react/containers/FetchedQuoteContainer.js
@@ -10,13 +10,23 @@ class FetchedQuoteContainer extends Component {
       htmlMood: '',
       yes: '',
       no: '',
+      clicked: false
     }
     this.handleOnClick = this.handleOnClick.bind(this)
     this.favoriteQuote = this.favoriteQuote.bind(this)
   }
 
   handleOnClick(event) {
-    let mood = event.target.innerHTML
+    let mood;
+    if(event.target.innerHTML == "Sad"){
+      mood = "happy"
+    } else if (event.target.innerHTML == "Unmotivated") {
+      mood = "motivational"
+    } else if (event.target.innerHTML == 'Uninspired') {
+      mood = "inspirational"
+    } else {
+      mood = this.state.htmlMood
+    }
     fetch(`/api/v1/fetched_quotes?category=${mood}`,{
       credentials: 'same-origin'
     })
@@ -35,15 +45,15 @@ class FetchedQuoteContainer extends Component {
         body: body.contents.quote,
         author: `- ${body.contents.author}`,
         htmlMood: mood,
-        yes: "yes",
-        no: "no"
+        yes: "Favorite it.",
+        no: "no",
+        clicked: true
       })
     })
     .catch(error => console.error(`Error in fetch: ${error.message}`));
     }
 
     favoriteQuote(event) {
-      debugger
       let formPayload = {
         body: this.state.body,
         author: this.state.author,
@@ -68,27 +78,47 @@ class FetchedQuoteContainer extends Component {
         throw(error);
       }
     })
-    .then(response => response.json())
+    .then(response => {
+      if(response.json()) {
+        alert("Saved successfully!")
+      }
+    })
     .catch(error => console.error(`Error in fetch: ${error.message}`))
   }
 
   render(){
-    return(
-      <div>
-        <button onClick={this.handleOnClick}>Sad</button>
-        <button onClick={this.handleOnClick}>Motivation</button>
-        <button onClick={this.handleOnClick}>Inspiration</button><br />
-        {this.state.catchError}
+    let hiddenDiv;
+
+    if (this.state.clicked) {
+      hiddenDiv = <div>
         <div className={this.state.htmlMood}>
           {this.state.body}<br />
           {this.state.author}
         </div>
-        <div className="favorite"onClick={this.favoriteQuote}>
+        <div className="feedback">
+          Did you like this quote?
+        </div>
+        <div className="favorite box" onClick={this.favoriteQuote}>
           {this.state.yes}
         </div>
-        <div className="dislike">
-          {this.state.no}
+        <div className="dislike box" onClick={this.handleOnClick}>
+          Generate new {this.state.htmlMood} quote.
         </div>
+      </div>
+    }
+
+    return(
+      <div>
+        <div className="intro">
+          Welcome to the Beginning Steps of a More Positive You! <br />
+          How are you feeling today?
+        </div>
+
+        <button onClick={this.handleOnClick}>Sad</button>
+        <button onClick={this.handleOnClick}>Unmotivated</button>
+        <button onClick={this.handleOnClick}>Uninspired</button><br />
+
+        {hiddenDiv}
       </div>
     )
   }
